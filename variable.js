9f9ba3ee90c29472d06c23b9ed7a89b99b7fa443
Jerom Ghagra <Jeromghagra@gmail.com>
2019-02-24 20:21:51 +0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,14 @@
+console.log("hello javascript");
+
+//Javascript varible
+let name;
+name = "Jerom Ghagra";
+
+let age;
+age = 23;
+
+console.log("my name is",name, "age", 23);
+
+//To declare a constant (unchanging) variable, use const instead of let:
+const my_name = "Jerom Ghagra";
+console.log(my_name);
